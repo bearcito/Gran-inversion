4e150daa3c30ff3b6570be1939a79998fd8b0f93
Nahuel Cabral <151670293+bearcito@users.noreply.github.com>
2024-03-06 02:12:05 -0300

    primer comit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css'
+import { CardPerson } from './pages/cardperson.js';
+
+function App() {
+  return (
+    <div className="App">
+      <CardPerson />
+    </div>
+  );
+}
+
+export default App;
